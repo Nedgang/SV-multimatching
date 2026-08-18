ce81b056ca1037639110ec45023e958b53017342
David Picard <pd8278@front.hpc.eskn.fr>
2026-08-18 10:55:39 +0200

    Refactoring for ease of reading and maintaining

--- sv_multimatching.py
+++ sv_multimatching.py
@@ -5,13 +5,13 @@ import argparse
 import polars as pl
 import pysam
 import pysam.bcftools
-import tempfile
 
 from utils.intervals_utils import (
     merged_intervals,
     overlap_size,
     is_list_intervals_in_limits,
 )
+from utils.vcf_bed_utils import read_vcf_as_bedfile
 
 ##########
 # PARSER #
@@ -102,30 +102,6 @@ def list_of_overlap_sv(
             )
             >= min_overlap
         ]
-
-
-def vcf_to_bedfile(vcf_path: str, bed_gz_path: str) -> None:
-    """ """
-    bed_path = tempfile.NamedTemporaryFile()
-    pysam.bcftools.query(
-        "-f",
-        "%CHROM\t%POS\t%END\t%ID",
-        "-o",
-        bed_path.name,
-        vcf_path,
-        catch_stdout=False,
-    )
-    pysam.tabix_compress(
-        filename_in=bed_path.name, filename_out=bed_gz_path, force=True
-    )
-    bed_path.close()
-    pysam.tabix_index(bed_gz_path, seq_col=0, start_col=1, end_col=2, force=True)
-
-
-def read_vcf_as_bedfile(file_path: str) -> pysam.TabixFile:
-    bedfile = tempfile.NamedTemporaryFile()
-    vcf_to_bedfile(file_path, bedfile.name)
-    return pysam.TabixFile(bedfile.name, parser=pysam.asBed())
 
 
 def is_there_multimatch(
