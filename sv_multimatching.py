de11ecba73566e78d639b7eff73f569322c8f614
David Picard <pd8278@front.hpc.eskn.fr>
2026-08-21 11:55:17 +0200

    Add debug info in the process

--- sv_multimatching.py
+++ sv_multimatching.py
@@ -76,7 +76,7 @@ parser.add_argument(
 )
 parser.add_argument(
     "-t",
-    "--tsv_path",
+    "--tsv_file",
     required=False,
     type=str,
     help="Path to tsv file for storing results.",
@@ -190,33 +190,36 @@ def main(args: argparse.ArgumentParser, logger: logging.Logger) -> None:
     logger.info("Checking input file")
     if args.input_file.endswith(".bed.gz"):
         logger.info(f"Input: {args.input_file} already in bed format")
-        sv_bed=pysam.TabixFile(args.input_file, parser=pysam.asBed())
+        sv_bed = pysam.TabixFile(args.input_file, parser=pysam.asBed())
         logger.debug(f"{args.input_file} read correctly ")
     else:
         logger.info(f"Converting {args.input_file} to bed format")
-        sv_bed=read_vcf_as_bedfile(args.input_file)
+        sv_bed = read_vcf_as_bedfile(args.input_file)
         logger.debug(f"Reading {args.input_file} in bed format")
     logger.info("Checking reference file")
     if args.reference.endswith(".bed.gz"):
         logger.info(f"Reference: {args.reference} already in bed format")
-        reference_bed=pysam.TabixFile(args.reference, parser=pysam.asBed())
+        reference_bed = pysam.TabixFile(args.reference, parser=pysam.asBed())
         logger.debug(f"{args.reference} read correctly ")
     else:
         logger.info(f"Converting {args.reference} to bed format")
-        reference_bed=read_vcf_as_bedfile(args.reference)
+        reference_bed = read_vcf_as_bedfile(args.reference)
         logger.debug(f"Reading {args.reference} in bed format")
 
     # Initialisation of the return dataframe:
+    logger.debug("Preparing output dataframe")
     output_dataframe = pl.DataFrame(
         [
             pl.Series("#Variant", [], dtype=pl.String),
             pl.Series("Reference", [], dtype=pl.String),
         ]
     )
+    logger.debug("Output dataframe initialized")
     # We need to keep trace of the chromosomes we work on.
     set_chr = set()
 
     # First from one variant at a time, search for all overlapping reference
+    logger.info("Launching multimatching on the variants from input")
     for sv in sv_bed.fetch():
         set_chr.add(sv.contig)
         list_ref_intervals = list_of_overlap_sv(
@@ -253,7 +256,9 @@ def main(args: argparse.ArgumentParser, logger: logging.Logger) -> None:
             )
 
     # Then, from one reference at a time, search for all overlapping variants
+    logger.info("Launching multimatching on the variants from reference")
     for chr in (contig for contig in set_chr if contig in reference_bed.contigs):
+        logger.debug(f"Working on {chr}")
         for ref in reference_bed.fetch(reference=chr):
             list_variants_intervals = list_of_overlap_sv(
                 bedfile=sv_bed,
@@ -289,7 +294,8 @@ def main(args: argparse.ArgumentParser, logger: logging.Logger) -> None:
                     )
                 )
     # Everything is done, now it's just display time.
-    if args.tsv_path is None:
+    logger.info("Multimatching finished for input and reference")
+    if args.tsv_file is None:
         print("#Variant\tReference")
         for i in [
             "\t".join((line["#Variant"], line["Reference"]))
@@ -297,12 +303,15 @@ def main(args: argparse.ArgumentParser, logger: logging.Logger) -> None:
         ]:
             print(i)
     else:
+        logger.info(f"Writing {args.tsv_file} result file")
         output_dataframe.unique().sort("#Variant").write_csv(
-            args.tsv_path, separator="\t"
+            args.tsv_file, separator="\t"
         )
+        logger.debug(f"{args.tsv_file} written")
 
     # If we want a list with all variants ID found in reference
     if args.list_variant_id is not None:
+        logger.info(f"Writing {args.list_variant_id}")
         with open(args.list_variant_id, "w") as file:
             file.write(
                 "\n".join(
@@ -316,11 +325,17 @@ def main(args: argparse.ArgumentParser, logger: logging.Logger) -> None:
                 )
             )
         file.close()
+        logger.debug(f"{args.list_variant_id} written")
+    logger.info("Job done, shutting done")
 
 
 if __name__ == "__main__":
     # Parser and logger instantation
     logger = logging.getLogger(__name__)
     args = parser.parse_args()
-    setup_logging(verbose=args.debug, log_file= args.journal_log)
+    setup_logging(verbose=args.debug, log_file=args.journal_log)
+    logger.debug(
+        f"Launching {parser.prog} with those parameters\n"
+        + "\n".join([f"{arg}: {str(getattr(args, arg))}" for arg in args.__dict__])
+    )
     main(args, logger)
